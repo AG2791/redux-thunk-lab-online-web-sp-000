73ad09b201e06e242f88efeebc7aa63dc7e382be
AJ Guiles <ashiguiles@gmail.com>
2020-06-09 04:13:11 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,4 +1,5 @@
 // write and export your action creator function here
+export const fetchCats = () => {
   return (dispatch) => {
     dispatch({ type: 'LOADING_CATS'})
     fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
